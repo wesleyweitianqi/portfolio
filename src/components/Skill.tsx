c70bf79586268ea32eb5e81ab565929dd7f1e3d1
wesleytianqiwei <wesleytianqiwei@gmail.com>
2022-11-08 16:14:30 -0500

    icon color

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -15,44 +15,44 @@ const Skill:React.FC = () => {
             <tr>
                 <td className="name">Language</td>
                 <td>
-                    <SiJavascript />
-                    <SiTypescript />
-                    <FaPython />
-                    <SiRuby />
-                    <SiHtml5 />
-                    <SiCss3 />
+                    <SiJavascript style={{color:"yellow"}}/>
+                    <SiTypescript style={{color:"#037ffc"}}/>
+                    <FaPython style={{color:"yellow"}}/>
+                    <SiRuby style={{color:"red"}}/>
+                    <SiHtml5 style={{color:"#fc8003"}}/>
+                    <SiCss3 style={{color:"#0384fc"}}/>
                 </td>
             </tr>
             <tr>
                 <td className="name">Front-End</td>
                 <td>
-                    <SiReact />
-                    <FaVuejs />
-                    <SiJquery />
-                    <FaSass />
-                    <SiBootstrap />
-                    <SiTailwindcss />
+                    <SiReact style={{color:"#03fcf8"}}/>
+                    <FaVuejs style={{color:"#03fc8c"}}/>
+                    <SiJquery style={{color:"#036ffc"}}/>
+                    <FaSass style={{color:"#fc0380"}}/>
+                    <SiBootstrap style={{color:"#a503fc"}}/>
+                    <SiTailwindcss style={{color:"#0398fc"}}/>
                 </td>
             </tr>
             <tr>
                 <td className="name">Back-End</td>
                 <td>
-                    <SiExpress />
-                    <FaNode />
-                    <SiRubyonrails />
-                    <SiPostgresql />
-                    <SiMongodb />
+                    <SiExpress style={{color:"#03fcb5"}}/>
+                    <FaNode style={{color:"#07fc03"}}/>
+                    <SiRubyonrails style={{color:"#fc1803"}}/>
+                    <SiPostgresql style={{color:"#0390fc"}}/>
+                    <SiMongodb style={{color:"#03fc6b"}}/>
                 </td>
             </tr>
             <tr>
                 <td className="name">Testing</td>
                 <td>
-                    <SiEslint />
-                    <SiMocha />
-                    <SiChai />
-                    <SiStorybook />
-                    <SiJest />
-                    <SiCypress />
+                    <SiEslint style={{color: "purple"}}/>
+                    <SiMocha style={{color: "#855f2e"}}/>
+                    <SiChai style={{color: "#a17132"}}/>
+                    <SiStorybook style={{color: "#cf2563"}}/>
+                    <SiJest style={{color: "#f0580c"}}/>
+                    <SiCypress style={{color: "#30332f"}}/>
                 </td>
             </tr>
         </table>
