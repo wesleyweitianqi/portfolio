f737a9b006364df4804c59404f3285b9daa0069f
wesleytianqiwei <wesleytianqiwei@gmail.com>
2022-11-10 11:46:18 -0500

    .env test

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -1,14 +1,26 @@
 export const projectList = [
   {
-    id: "tinyapp",
+    id: "Tinyapp",
     discription: "TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLs",
     stacks: ["express", "ejs", "bootstrap", "bcryptjs"],
-    image:"https://github.com/wesleyweitianqi/tinyapp/raw/main/docs/Monosnap%20screencast%202022-11-09%2021-50-55.gif?raw=true"
-},
+    image: "https://github.com/wesleyweitianqi/tinyapp/raw/main/docs/Monosnap%20screencast%202022-11-09%2021-50-55.gif?raw=true"
+  },
   {
-    id: "tinyapp",
-    discription: "TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLs",
-    stacks: ["express", "ejs", "bootstrap", "bcryptjs"],
-    image:"https://github.com/wesleyweitianqi/tinyapp/raw/main/docs/Monosnap%20screencast%202022-11-09%2021-50-55.gif?raw=true"
-},
+    id: "N-chat",
+    discription: "A realtime chatRoom application built with Express,React and MongoDB",
+    stacks: ["React", "socket.io", "sass", "buffer"],
+    image: "https://github.com/wesleyweitianqi/N-Chat/blob/master/doc/Chat.gif?raw=true"
+  },
+  {
+    id: "BuySellListing",
+    discription: "Buy & Sell is a buy-and-sell app built with Node.js, Express and postgreSQL. ",
+    stacks: ["Node.js", "Express", "Ejs", "postgresql"],
+    image: "https://github.com/wesleyweitianqi/scheduler/blob/master/docs/appointment%20day%20select%20in%20hover.png?raw=true"
+  },
+  {
+    id: "Jungler",
+    discription: "A mini e-commerce application built with Rails 6.1 for purposes of teaching Rails by example.",
+    stacks: ["Ruby on rails", "stripe", "postgresql", "bootstrap"],
+    image: "https://github.com/wesleyweitianqi/Jungle/blob/master/doc/payment.png?raw=true"
+  },
 ]
