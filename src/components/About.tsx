b640f258c0cd6ed3e64c4139ed0587aa0654c79e
wesleyweitianqi <wesleytianqiwei@gmail.com>
2022-11-27 21:27:00 -0500

    update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,34 +1,36 @@
 import React from "react";
 import "./about.scss";
 import profile from "../assets/Profile.jpg";
-import { MdLocationOn } from 'react-icons/md'
+import { MdLocationOn } from "react-icons/md";
 
 const About: React.FC = () => {
   return (
     <div id="about" className="island">
-    <div className="space"></div>
-    <div className="about-container" data-name="about">
-      <div id="brief">
-        <p id="topic">Hello</p>
-        <p>
-          Analytical Full stack Web Developer with a background in Mechanical
-          Engineering. I love learning new technologies and converting data into
-          visualization that has an active interface that is useful to the end
-          users that drew me into web development. I enjoy creating applications
-          that are functional, cool, unique and helpful the the daily lives of
-          customers.
-        </p>
-      </div>
-      <div className="profile">
-        <img src={profile} alt="profile" />
-        <div className="location"><MdLocationOn />
-        <span> Toronro Canada</span>
+      <div className="space"></div>
+      <div className="about-container" data-name="about">
+        <div id="brief">
+          <p id="topic">Hello</p>
+          <p>
+            My name is Wesley Wei, in search of a career in full-stack or
+            front-end development. I am eager to continue learning and
+            collaborate with other developers of various experience levels. As a
+            graduate from <a className="lhl" href="https://www.lighthouselabs.ca/">Lighthouse Labs</a>, I was involved in a
+            fast-paced learning environment comprised of small to large projects
+            using popular front and back-end languages. I also transitioned from
+            the power distribution industry as a design engineer and programmer
+            at an transformer company with transferable skills in
+            problem-solving and project management that will help to kickstart a
+            new career in programming!
+          </p>
         </div>
-        
+        <div className="profile">
+          <img src={profile} alt="profile" />
+          <div className="location">
+            <MdLocationOn />
+            <span> Toronto Canada</span>
+          </div>
+        </div>
       </div>
-    </div>
-    
-    
     </div>
   );
 };
